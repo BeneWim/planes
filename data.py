1437f6a0679dbeec5401fb524570b143eeff1c20
Benedikt Wimmer <benedikt.wimmer@aqarios.com>
2024-12-08 16:58:45 +0200

    Update data generation

--- data.py
+++ data.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class Data:
     def __init__(
         self,
@@ -9,9 +8,10 @@ class Data:
         T: int = 20,
         distribution: str = "normal",
         max_time: int = 5,
-        sum_to_T: bool = True,
+        sum_to_T: bool = False,
         equal_assignment: bool = False,
         seed: int = 42,
+        team_limit: int = 5,
     ) -> None:
         self.num_teams = num_teams
         self.num_bells = num_bells
@@ -21,6 +21,7 @@ class Data:
         self.sum_to_T = sum_to_T
         self.equal_assignment = equal_assignment
         self.seed = seed
+        self.team_limit = team_limit
 
         self.plane_services = []
 
@@ -37,7 +38,7 @@ class Data:
             if self.sum_to_T:
                 return self.random_integers_with_fixed_T()
             else:
-                NotImplementedError()
+                return self.random_integers_with_fixed_bells()
         else:
             raise ValueError("distribution not supported")
 
@@ -67,5 +68,25 @@ class Data:
 
         if remaining_sum > 0:
             numbers.append(remaining_sum)
+
+        return numbers
+
+    def random_integers_with_fixed_bells(self):
+        numbers = []
+        remaining_sum = self.T
+
+        for _ in range(self.num_bells + 1):
+            random_number = self.generator.integers(
+                1, min(remaining_sum, self.team_limit)
+            )
+
+            if remaining_sum - random_number <= 1:
+                numbers.append(remaining_sum)
+
+                break
+            else:
+                numbers.append(random_number)
+
+            remaining_sum -= random_number
 
         return numbers
